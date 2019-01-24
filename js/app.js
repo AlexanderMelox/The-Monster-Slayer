08c25a13cc313235fac46cc4eba9e7dfee7c8530
Alexander Melo <alexander.melo@ibm.com>
2019-01-24 11:26:05 -0600

    added template for battle log also added the attack method and updateBattlelog method

--- js/app.js
+++ js/app.js
@@ -4,6 +4,17 @@ new Vue({
     isPlaying: false,
     playerHealth: 100,
     monsterHealth: 100,
+    log: [{
+      character: 'player',
+      action: 'hits',
+      number: 12
+    }],
+    settings: {
+      attack: {
+        minDmg: 3,
+        maxDmg: 13
+      }
+    }
   },
   methods: {
     startGame: function () {
@@ -17,10 +28,28 @@ new Vue({
       this.playerHealth = 100;
       // Sets the monster's health back to 100
       this.monsterHealth = 100;
+      // Sets the battle log to an empty array
+      this.log = [];
     },
     attack: function () {
-      console.log('ATTACK');
-      console.log(this.getRandomNumber(5, 13));
+      // 1. Generates a random attack number for the player and the monster
+      const playerDmg = this.getRandomNumber(this.settings.attack.minDmg, this.settings.attack.maxDmg);
+      const monsterDmg = this.getRandomNumber(this.settings.attack.minDmg, this.settings.attack.maxDmg);
+      // 2. Reduce the dmg done by the character
+      this.playerHealth -= monsterDmg;
+      this.monsterHealth -= playerDmg;
+      // 3. Update the battle log
+      this.updateBattleLog({
+        type: 'attack',
+        player: playerDmg,
+        monster: monsterDmg
+      });
+    },
+    updateBattleLog: function (options) {
+      const { type } = options;
+      if (type === 'attack') {
+        console.log(type);
+      }
     },
     getRandomNumber: function (min, max) {
       // Get a number between min (inclusive) and max (exclusive)
