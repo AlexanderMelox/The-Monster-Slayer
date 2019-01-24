9711a961c57019c4536bfaabe3d2811c1e2e3e52
Alexander Melo <alexander.melo@ibm.com>
2019-01-24 10:29:16 -0600

    added startGame and reset function, also added computed properties for monster and player health styling

--- js/app.js
+++ js/app.js
@@ -3,9 +3,30 @@ new Vue({
   data: {
     isPlaying: false,
     playerHealth: 100,
-    monsterHealth: 100
+    monsterHealth: 100,
   },
   methods: {
-
+    startGame: function () {
+      // 1. Turn isPlaying to true to start game
+      this.isPlaying = true;
+      // 2. Reset values when re-starting a game
+      this.reset();
+    },
+    reset: function () {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    }
+  },
+  computed: {
+    playerHealthBarStyles: function () {
+      return {
+        width: this.playerHealth + '%'
+      }
+    },
+    monsterHealthBarStyles: function () {
+      return {
+        width: this.monsterHealth + '%'
+      }
+    }
   }
 });
